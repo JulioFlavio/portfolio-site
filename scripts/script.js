273c498fda809f2f3df85841a6bf690f1e321e31
Júlio Júnior <julioflaviojunior.bh@gmail.com>
2025-05-26 20:43:04 -0300

    Seção 'Meus Projetos' feita com API do GitHub adicionada

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-var arq = null
 async function pesquisaJson() {
   await fetch("db/habilidades.json")
   .then((response) => {
@@ -33,3 +32,29 @@ function substituiNivel(minhasHabilidades) {
 }
 
 pesquisaJson()
+// chama também "subsituiHabilidades" e "substituiNivel"
+
+async function consultaRepositoriosGithub() {
+  const resposta = await fetch("https://api.github.com/users/JulioFlavio/repos")
+  const github = await resposta.json()
+  
+  console.log(github)
+  github.forEach(repositorio => {
+    document.getElementById("cards").innerHTML += `
+      <div class="max-w-sm p-6 my-7 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+          <a href="${repositorio.html_url}">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" target="_blank">${repositorio.name}</h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${repositorio.description}</p>
+          <a href="${repositorio.html_url}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Ver no GitHub
+              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+              </svg>
+          </a>
+      </div>
+    `
+  });
+}
+
+consultaRepositoriosGithub()
