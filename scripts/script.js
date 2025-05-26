9f7421a879910a7fe82c1c3dc1dfdbff1ade5c60
Júlio Júnior <julioflaviojunior.bh@gmail.com>
2025-05-26 19:35:51 -0300

    Itens incluídos na seção 'Sobre Mim' usando JavaScript (os itens de 'Nivel de Conhecimento' estão com um erro de display, ajustar)

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,35 @@
-async function habList() {
+var arq = null
+async function pesquisaJson() {
   await fetch("db/habilidades.json")
   .then((response) => {
-    response.json().then((data) => {
-      console.log(data)
-      // fazer tudo aqui dentro do fetch, não consegui atribuir o fetch a uma variavel
-
-      document.getElementById("habilidades").innerHTML += ``
-
-      for (let i = 0; i < data.habilidades.length; i++) {
-        document.getElementById("habilidades").innerHTML += `
-          <span class="habilidades">${data.habilidades[i].nome}</span>
-        `
-      }
-
-      
-      
-    })
+    return response.json();
   })
+  .then(data => {
+    
+    substituiNivel(data.habilidades)
+    substituiHabilidades(data.habilidades)
+
+  })
+}
+
+function substituiHabilidades(minhasHabilidades) {
+  minhasHabilidades.forEach(habilidade => {
+    document.getElementById("habilidades").innerHTML += `
+      <span class="habilidades">${habilidade.nome}</span>
+    `});
+}
+
+function substituiNivel(minhasHabilidades) {
+  minhasHabilidades.forEach(habilidade => {
+    document.getElementById("nivel").innerHTML += `
+      <div class="my-5 p-3 flex flex-wrap justify-between text-neutral-50 rounded-xl bg-linear-to-r from-${habilidade.cor1}-${habilidade.tom1} to-${habilidade.cor2}-${habilidade.tom2}">
+        <h1 class="text-lg">${habilidade.icon} ${habilidade.nome}</h1>
+        <span class="text-md justify-self-end">${habilidade.nivel}</span>
+        <p class="text-sm">${habilidade.descricao}</p>
+      </div>
+    `
+  });
 
 }
 
-onload(habList());
+pesquisaJson()
