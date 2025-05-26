d43d04dbd78e96bbf55fc6d869838f446c1b8d85
Júlio Júnior <julioflaviojunior.bh@gmail.com>
2025-05-26 20:01:05 -0300

    Itens incluídos na seção 'Sobre Mim' usando JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,8 @@ async function pesquisaJson() {
   })
   .then(data => {
     
-    substituiNivel(data.habilidades)
     substituiHabilidades(data.habilidades)
+    substituiNivel(data.habilidades)
 
   })
 }
@@ -24,8 +24,8 @@ function substituiNivel(minhasHabilidades) {
     document.getElementById("nivel").innerHTML += `
       <div class="my-5 p-3 flex flex-wrap justify-between text-neutral-50 rounded-xl bg-linear-to-r from-${habilidade.cor1}-${habilidade.tom1} to-${habilidade.cor2}-${habilidade.tom2}">
         <h1 class="text-lg">${habilidade.icon} ${habilidade.nome}</h1>
-        <span class="text-md justify-self-end">${habilidade.nivel}</span>
-        <p class="text-sm">${habilidade.descricao}</p>
+        <span class="text-md bg-neutral-100/30 p-2 m-1 rounded-xl justify-self-end">${habilidade.nivel}</span>
+        <p class="text-sm font-bold">${habilidade.descricao}</p>
       </div>
     `
   });
